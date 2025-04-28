422de1b4a2730f75ae7cb1754e1de9244ec9f462
vZiad <ziad.mc77@gmail.com>
2025-04-28 22:37:30 +0300

    Create PhotoManager.java

---
+++ PhotoManager.java
@@ -0,0 +1,59 @@
+public class PhotoManager {
+    LinkedList<Photo> allPhotos;
+
+    public PhotoManager() {
+        allPhotos = new LinkedList<Photo>();
+    }
+
+    public void addPhoto(Photo p) {
+    	
+        if (PhotoExist(p))  // to prevent repeating, we create method "PhotoExist" to check if it's in the LinkedList of photos
+        	return;              
+        
+        allPhotos.insert(p);
+    }
+    public void deletePhoto(String path) {
+    	
+    	if(allPhotos.empty())
+    		return;
+    	
+    	allPhotos.findFirst();
+    	
+    	while(! allPhotos.last()){
+    		
+    		if(allPhotos.retrieve().path.equals(path)) {
+    			
+    			allPhotos.remove();
+    			
+    			break;
+    		}
+    		else
+    			allPhotos.findNext();
+    	}
+    	if(allPhotos.retrieve().path.equals(path))  //Check Last Node
+    		allPhotos.remove();
+    }
+    
+    public LinkedList<Photo> getPhotos(){
+    	return allPhotos;
+    }
+    
+    // ====== Helping Method =========
+    public boolean PhotoExist(Photo p) {
+    	
+        if (allPhotos.empty()) return false;
+        allPhotos.findFirst();
+        
+        while (!allPhotos.last()) {
+        	
+            if (allPhotos.retrieve().path.equals(p.path))
+                return true;
+            allPhotos.findNext();
+        }
+        
+        if (allPhotos.retrieve().path.equals(p.path)) //Check Last Node
+            return true;
+        
+        return false;
+    }
+}
