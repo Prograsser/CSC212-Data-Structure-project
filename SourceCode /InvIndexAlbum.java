699c68c6c9b4ec1fdf5985f3228ac284030bdd3d
Yasser AL.Asiri <63279176+Prograsser@users.noreply.github.com>
2025-04-29 04:50:12 +0300

    Update InvIndexAlbum.java

--- SourceCode /InvIndexAlbum.java
+++ SourceCode /InvIndexAlbum.java
@@ -1,45 +1,48 @@
-
 public class InvIndexAlbum {
 	private String name;
 	private String condition;
 	private InvIndexPhotoManager manager;
 
-	// Constructor
+	// Constructor: Initializes the album with a name, condition, and photo manager
 	public InvIndexAlbum(String name, String condition, InvIndexPhotoManager manager) {
 		this.name = name;
 		this.condition = condition;
 		this.manager = manager;
 	}
 
-	// Return the name of the album
+	// Getter: Returns the name of the album
 	public String getName() {
 		return name;
 	}
 
-	// Return the condition associated with the album
+	// Getter: Returns the condition associated with the album
 	public String getCondition() {
 		return condition;
 	}
 
-	// Return the manager
+	// Getter: Returns the manager responsible for handling photos
 	public InvIndexPhotoManager getManager() {
 		return manager;
 	}
 
-	// Return all photos that satisfy the album condition
+	// Returns all photos that satisfy the album's condition
 	public LinkedList<Photo> getPhotos() {
-
 		LinkedList<Photo> res = new LinkedList<>();
 
+		// If no condition is set, return an empty list
 		if (condition == null) return res;
+		// If the condition is empty, return all photos
 		if (condition.equals("")) return manager.allPhotos;
 
+		// Split condition into multiple tags connected by "AND"
 		String cond[] = condition.split("AND");
 		for (int i = 0; i < cond.length; i++) {
 			cond[i] = cond[i].trim();
 		}
 		
+		// Get photos matching the first tag
 		LinkedList<Photo> A = getTagPhotos(cond[0]);
+		// Intersect with photos matching subsequent tags
 		for(int i = 1; i < cond.length; i++) {
 			System.out.println("a[i] = " + cond[0]);
 			LinkedList<Photo> B = getTagPhotos(cond[i]);
@@ -48,12 +51,14 @@ public class InvIndexAlbum {
 		return A;
 	}
 
-	// Return the number of tag comparisons used to find all photos of the album
+	// Returns the number of tag comparisons performed when retrieving photos
 	public int getNbComps() {
 		return manager.index.num_comp;
 	}
 
-	// =====Helping methods========
+	// ===== Helping methods =====
+
+	// Checks if a given photo exists in the provided list
 	public boolean IsPhotoExist(LinkedList<Photo> l, Photo p) {
 		if (l.empty())
 			return false;
@@ -63,13 +68,14 @@ public class InvIndexAlbum {
 				return true;
 			l.findNext();
 		}
+		// Check the last element
 		if (l.retrieve().path.equals(p.path))
 			return true;
 
 		return false;
-
 	}
 
+	// Retrieves all photos that match a specific tag
 	public LinkedList<Photo> getTagPhotos(String tag) {
 		LinkedList<Photo> res = new LinkedList<Photo>();
 		boolean flag = manager.getPhotos().findKeyPhoto(tag);
@@ -78,12 +84,14 @@ public class InvIndexAlbum {
 		return res;
 	}
 
+	// Returns the intersection of two photo lists (photos common to both lists)
 	public LinkedList<Photo> comparePhoto(LinkedList<Photo> a, LinkedList<Photo> b) {
 		LinkedList<Photo> res = new LinkedList<Photo>();
 		if (a.empty() || b.empty())
 			return res;
 		a.findFirst();
 		while (true) {
+			// Check if photo already added to result
 			if (!IsPhotoExist(res, a.retrieve())) {
 				b.findFirst();
 				while (true) {
@@ -97,7 +105,6 @@ public class InvIndexAlbum {
 						break;
 				}
 			}
-
 			if (!a.last())
 				a.findNext();
 			else
