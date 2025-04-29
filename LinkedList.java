765bbb36f20e76f22fc18dc4f8e0b9f34bcc99f3
Yasser AL.Asiri <63279176+Prograsser@users.noreply.github.com>
2025-04-29 03:54:31 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,65 +1,107 @@
-class Node<T> { 
-    T data;
-    Node<T> next;
-    public Node(T data){
-        this.data=data;
-        this.next=null;
-    }
+public class Node<T> {
+	public T data;
+	public Node<T> next;
 
+	public Node() {
+		data = null;
+		next = null;
+	}
+
+	public Node(T val) {
+		data = val;
+		next = null;
+	}
 }
 
 public class LinkedList<T> {
-    private Node<T> head;
-    private Node<T> current;
-    public LinkedList(){
-        head=null;
-        current=null;
-    }
-    public void findFirst(){
-        current=head;
-    }
-    public boolean empty(){
-        return head==null;
-    }
-    public boolean last(){
-        return current.next==null;
-    }
-    public void insert(T data){
-        Node<T> tmp;
-        if (empty()){
-           current=head=new Node<T>(data);
-        }
-        else{
-         tmp=current.next;
-         current.next=new Node<T>(data);
-         current.next=tmp;
-        }
-    }
-    public void remove(){
-        if (current==head){
-            head=head.next;
-        }
-        else{
-            Node<T> tmp=head;
-            while (tmp.next!=current)
-                tmp=tmp.next;
-                tmp.next=tmp.next;
-            }
-        if (current.next==null){
-            current=head;
-        }
-        else{
-            current=current.next;    
-        }
-    }
-    public int length(){
-        int count=0;
-        Node<T> tmp=head;
-        while (tmp!=null){
-            count++;
-            tmp=tmp.next;
-        }
-        return count;
-    }
+
+	private Node<T> head;
+	private Node<T> current;
+
+	public LinkedList() {
+		head = current = null;
+	}
+
+	public boolean full() {
+		return false;
+	}
+
+	public boolean last() {
+		return current.next == null;
+	}
+
+	public void findFirst() {
+		current = head;
+	}
+
+	public boolean empty() {
+		return head == null;
+	}
+
+	public void findNext() {
+		current = current.next;
+	}
+
+	public void update(T e) {
+		current.data = e;
+	}
+
+	public T retrive() {
+		return current.data;
+	}
+
+	public void insert(T e) {
+		Node<T> p = new Node<T>(e);
+		if (head == null)
+			head = current = p;
+		else {
+			Node<T> tmp = current.next;
+
+			current.next = p;
+
+			p.next = tmp;
+
+			current = p;
+		}
+	}
+
+	public void remove() {
+		if (current == head) {
+			head = head.next;
+			current = head;
+		} else {
+			Node<T> tmp = head;
+			while (tmp != current)
+				tmp = tmp.next;
+			tmp.next = current.next;
+
+		}
+		if (current.next == null)
+			current = head;
+		else
+			current = current.next;
+
+	}
+	 public int length(){
+	        int count=0;
+	        Node<T> tmp=head;
+	        while (tmp!=null){
+	            count++;
+	            tmp=tmp.next;
+	        }
+	        return count;
+	    }
+//helping Methods
+	public void displayList() {
+		Node<T> tmp = head;
+		if (empty())
+			return;
+		while (tmp != null) {
+			System.out.println(tmp.data);
+			tmp = tmp.next;
+		}
+	}
 }
+
+
 
